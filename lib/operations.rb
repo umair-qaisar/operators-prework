dd120bd99e46ee386091b440940a843fe4000aa5
qaisaru@icloud.com <qaisaru@icloud.com>
2018-09-11 07:13:03 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,11 @@
 def unsafe?(speed)
-  if speed < 30 "unsafe"
-  elsif speed > 60 "unsafe"
-  else "safe"
-
-
+   if speed>=40 && speed<=60
+    false
+  else
+    true
+  end
 end
 
-
-
 def not_safe?(speed)
-
+			speed<=40 || speed>=60 ? true : false
 end
